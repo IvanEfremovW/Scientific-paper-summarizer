9de21ca6f6d78b7eb0b87b508a650113b0896eff
IvanEfremovW <Ivan.EfremovWM@yandex.ru>
2025-10-25 19:13:25 +0300

    feat: incapsulate entry point
    - move gradio logic to main function
    - rename summarization pipeline function from main to pipeline

--- src/summarizer/app.py
+++ src/summarizer/app.py
@@ -4,19 +4,25 @@ from pathlib import Path
 from summarizer.summarizer import Summarizer
 from summarizer.ingestion import exctract_text_from_document
 
-def main(docuement_path: str | Path) -> str:
+
+def pipeline(docuement_path: str | Path) -> str:
     summarizer = Summarizer()
-   
+
     text = exctract_text_from_document(docuement_path)
     summary = summarizer.summarize(text)
 
     return summary
-    
 
-interface = gr.Interface(
-    fn=main,
-    inputs=gr.File(label="Upload a File"), 
-    outputs=gr.Textbox(label="Summary", lines=25),
-)
 
-interface.launch()
+def main():
+    interface = gr.Interface(
+        fn=pipeline,
+        inputs=gr.File(label="Upload a File"),
+        outputs=gr.Textbox(label="Summary", lines=25),
+    )
+
+    interface.launch()
+
+
+if __name__ == "__main__.py":
+    main()
